3b88c22400c4ee86ec980cc8d3929177b6a91f24
Smilla Sollén <ss226uk@student.lnu.se>
2025-09-02 16:29:36 +0200

    Setup express server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Smilla Sollén <ss226uk@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express';
+
+const app = express();
+const PORT = 3015;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
